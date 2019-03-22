63d656c6eb216ac599f2fc9dded31958fb617176
Steven Knox <steven@wholeschool.ie>
2019-03-22 12:39:25 +0000

    Turn off GenerateAssemblyInfo and use original files. Update Redis lib to support netstandard2.0 and net451

--- src/Takenet.Textc/Assembly.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-using Takenet.Textc.Metadata;
-
-[assembly:TokenTypeLibraryAttribute] //STEVEN - is this correct ?
-[assembly:InternalsVisibleTo("Takenet.Textc.UnitTests")]
---
+++ src/Takenet.Textc/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Takenet.Textc.Metadata;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Takenet.Textc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Takenet.Textc")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Take.net")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("875df2a3-f0cd-4245-820b-ca0caf0565a6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: TokenTypeLibrary]
+[assembly: InternalsVisibleTo("Takenet.Textc.UnitTests")]
